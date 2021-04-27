ce61f0ff404187f02919c5b9d10fc5cb81f266db
Benjamin Custo <bcusto89@gmail.com>
2021-04-27 23:02:28 +0000

    added deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t benjamincusto/multi-client:latest -t benjamincusto/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t benjamincusto/multi-server:latest -t benjamincusto/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t benjamincusto/multi-worker:latest -t benjamincusto/multi-worker:$SHA -f ./client/Dockerfile ./worker
+docker build -t benjamincusto/multi-worker:latest -t benjamincusto/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push benjamincusto/multi-client:latest
 docker push benjamincusto/multi-server:latest
 docker push benjamincusto/multi-worker:latest
@@ -12,6 +12,6 @@ docker push benjamincusto/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=benjamincusto/multi-server:$SHA
-kubectl set image deployments/client-deployment server=benjamincusto/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=benjamincusto/worker-client:$SHA
+kubectl set image deployments/client-deployment client=benjamincusto/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=benjamincusto/worker-client:$SHA
 
